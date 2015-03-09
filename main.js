d621b1e737be83e1c3f73e93e078e9ee9066d07c
Paul Laskowski <pelaskowski@gmail.com>
2015-03-09 15:12:40 -0700

    added game controls to start new, or leave a game

--- main.js
+++ main.js
@@ -1,26 +1,68 @@
 Games = new Meteor.Collection('games');
+Players = new Meteor.Collection('players');
+Devices = new Meteor.Collection('devices');
 
 if (Meteor.isClient) {
   
-  Template.NewGame.events ({
+  Meteor.startup(function() {
+    if (localStorage.getItem('deviceId') === null) {
+      //if no deviceId, then try to get it from gameId
+      localStorage.setItem('deviceId', Devices.insert({nickname: ''}));
+    }
+
+    Session.set('deviceId', localStorage.deviceId);
+    Session.set('gameId', localStorage.gameId);
+    
+  });
+  
+  Template.GameControl.helpers ({
+    gamesWaiting: function() {
+      return Games.find({players: {$size: 1}});
+    },
+    
+    gameInProgress: function() {
+      return Session.get('gameId');
+    }
+  });
+  
+  Template.GameControl.events ({
     'click #newGame': function() {
       var cards = [];
       for (i=1; i<=20; i++) {
         cards.push(i);
       }
+      // make a random shuffle of all pairs
       var cardsDbl = cards.concat(cards);
       var cardsObj = shuffle(cardsDbl).map(function(value, index) {
         return {'idx': index, 'val': value, 'class': 'turned-down'};
       });
-      localStorage.setItem('gameId', Games.insert({grid: cardsObj}));
+      
+      // initialize new game entry in db
+      localStorage.setItem('gameId', Games.insert({
+        grid: cardsObj,
+        players: [localStorage.deviceId],
+        timestamp: Date.now()
+      }));
+      Session.set('gameId', localStorage.gameId);
+    },
+    
+    'click #leaveGame': function() {
+      var conf = window.confirm('Really? End this game?');
+      if (conf == true) {
+        Meteor.call('removeMyGames', Session.get('gameId'), Session.get('deviceId'));
+      }
+      Session.set('gameId', null);
+      localStorage.setItem('gameId', null);
     }
   });
   
   Template.Grid.helpers ({
     shuffledCards: function() {
-      var curGameId = localStorage.gameId;
-      var game = Games.findOne({_id: curGameId});
-      return game.grid;
+      if (Session.get('gameId')) {
+        var curGameId = Session.get('gameId');
+        var game = Games.findOne({_id: curGameId});
+        return game.grid;
+      }
     }
   });
 
@@ -33,6 +75,15 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+  
+  Meteor.methods ({
+    removeMyGames: function(gameId, deviceId) {
+      Games.remove({
+        _id: gameId,
+        'players.0': deviceId
+      })
+    }
+  })
 }
 
 
